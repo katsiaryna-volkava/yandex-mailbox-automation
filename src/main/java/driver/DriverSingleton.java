d71dee8c6ca8cc70045ef718f471355b20f312fc
katsiaryna-volkava <volkava.katsiaryna@gmail.com>
2019-09-10 15:38:32 +0300

    adding webdriver manager library and refactoring

--- src/main/java/driver/DriverSingleton.java
+++ src/main/java/driver/DriverSingleton.java
@@ -1,26 +1,26 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
 
-    private static final String RESOURCES_PATH = "src\\test\\resources\\";
     private static WebDriver driver;
 
+    private DriverSingleton() {
+    }
 
-    private DriverSingleton(){}
-
-    public static WebDriver getDriver(){
-        if (null == driver){
-            switch (System.getProperty("browser")){
+    public static WebDriver getDriver() {
+        if (null == driver) {
+            switch (System.getProperty("browser")) {
                 case "firefox": {
-                    System.setProperty("webdriver.gecko.driver", RESOURCES_PATH + "geckodriver.exe");
+                    WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
                 }
                 default: {
-                    System.setProperty("webdriver.chrome.driver", RESOURCES_PATH + "chromedriver.exe");
+                    WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                 }
             }
@@ -29,7 +29,7 @@ public class DriverSingleton {
         return driver;
     }
 
-    public static void closeDriver(){
+    public static void closeDriver() {
         driver.quit();
         driver = null;
     }
