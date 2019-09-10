65481431849a170073fc20f62b1b1cdb955426b6
katsiaryna-volkava <volkava.katsiaryna@gmail.com>
2019-09-10 15:26:54 +0300

    adding singleton for driver manager and smoke xml suite

--- src/test/java/SetUpAndTearDown.java
+++ src/test/java/SetUpAndTearDown.java
@@ -1,6 +1,8 @@
+import driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
+
 import java.util.concurrent.TimeUnit;
 
 public class SetUpAndTearDown {
@@ -8,27 +10,20 @@ public class SetUpAndTearDown {
     protected WebDriver driver;
 
     private static final String LOGIN_PAGE_URL = "https://mail.yandex.by";
-    private static final String GOOGLE_CHROME_DRIVER_PATH = ".//src/main/resources/chromedriver.exe";
-    private static final String CHROME_DRIVER_PROPERTY = "webdriver.chrome.driver";
 
     @BeforeMethod
     public void browserSetup() {
-        prepareChromeBrowser();
+        driver = DriverSingleton.getDriver();
         maximazeBrowser();
         String timeout = System.getProperty("Timeout");
         waitImplicitly(driver, Integer.valueOf(timeout));
         goToLoginPage();
-        System.out.println(timeout + "- timeout");
     }
 
     void goToLoginPage() {
         driver.get(LOGIN_PAGE_URL);
     }
 
-    void prepareChromeBrowser() {
-        System.setProperty(CHROME_DRIVER_PROPERTY, GOOGLE_CHROME_DRIVER_PATH);
-        driver = new ChromeDriver();
-    }
 
     void maximazeBrowser() {
         driver.manage().window().maximize();
@@ -40,6 +35,6 @@ public class SetUpAndTearDown {
 
     @AfterMethod
     public void browserClose() {
-        driver.quit();
+        DriverSingleton.closeDriver();
     }
 }
