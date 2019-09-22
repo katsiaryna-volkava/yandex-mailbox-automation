4b3441d07a9734f948dd3795a8f85320890f254e
katsiaryna-volkava <volkava.katsiaryna@gmail.com>
2019-09-22 13:28:16 +0300

    sode smell: Utility classes should not have public constructors

--- src/main/java/service/TestDataReader.java
+++ src/main/java/service/TestDataReader.java
@@ -3,6 +3,10 @@ package service;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
+
+    private TestDataReader() {
+    }
+
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 
     public static String getTestData(String key){
