531145bb3b8ce645cd7f9d53535b1bdc34167d84
katsiaryna-volkava <volkava.katsiaryna@gmail.com>
2019-09-22 14:13:06 +0300

    sode smell: Deprecated methods should be removed

--- src/main/java/utils/WaitUtils.java
+++ src/main/java/utils/WaitUtils.java
@@ -2,21 +2,30 @@ package utils;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.*;
-import java.util.concurrent.TimeUnit;
+
+import java.time.Duration;
 
 public class WaitUtils {
 
+    private WaitUtils() {
+    }
+
     public static void waitUntilVisibilityOfElementLocatedBy(WebDriver driver, By by) {
-        Wait wait = new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS)
-                .pollingEvery(1, TimeUnit.SECONDS)
+        Wait wait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
                 .ignoring(NoSuchElementException.class)
                 .ignoring(ElementClickInterceptedException.class)
                 .ignoring(UnhandledAlertException.class);
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
+
+
+
     public static void waitUntilVisibilityOfElement(WebDriver driver, WebElement webElement) {
-        Wait wait = new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS)
-                .pollingEvery(1, TimeUnit.SECONDS)
+        Wait wait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
                 .ignoring(NoSuchElementException.class)
                 .ignoring(ElementClickInterceptedException.class)
                 .ignoring(UnhandledAlertException.class);
@@ -24,8 +33,9 @@ public class WaitUtils {
     }
 
     public static void waitUntilClickabilityOfElement(WebDriver driver, WebElement webElement) {
-        Wait wait = new FluentWait(driver).withTimeout(10, TimeUnit.SECONDS)
-                .pollingEvery(1, TimeUnit.SECONDS)
+        Wait wait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
                 .ignoring(NoSuchElementException.class)
                 .ignoring(ElementClickInterceptedException.class)
                 .ignoring(UnhandledAlertException.class);
