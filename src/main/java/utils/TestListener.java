9789114901ebcdd8846c07200d41fa175c22595d
katsiaryna-volkava <volkava.katsiaryna@gmail.com>
2019-09-22 13:57:21 +0300

    sode smell: String formatters should be used instead of string concatenation

--- src/main/java/utils/TestListener.java
+++ src/main/java/utils/TestListener.java
@@ -12,29 +12,38 @@ import org.testng.ITestResult;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class TestListener implements ITestListener {
     private Logger log = LogManager.getRootLogger();
 
+    String testName = null;
+
     public void onTestStart(ITestResult iTestResult) {
+        testName = iTestResult.getMethod().getMethodName();
         log.info("----------------------------------------------------------------------------");
-        log.info("Test with name: " + iTestResult.getMethod().getMethodName() + " has started");
+        log.info(String.format("Test %s has started", testName));
     }
 
     public void onTestSuccess(ITestResult iTestResult) {
-        log.info("Test with name: " + iTestResult.getMethod().getMethodName() + " has passed");
+        testName = iTestResult.getMethod().getMethodName();
+        log.info(String.format("Test %s has started", testName));
 
     }
 
     public void onTestFailure(ITestResult iTestResult) {
+        testName = iTestResult.getMethod().getMethodName();
         saveScreenshot();
-        log.info("Test with name: " + iTestResult.getMethod().getMethodName() + " has failed");
+        log.info(String.format("Test %s has failed", testName));
     }
 
     public void onTestSkipped(ITestResult iTestResult) {
-        log.info("Test with name: " + iTestResult.getMethod().getMethodName() + " has skipped");
+        testName = iTestResult.getMethod().getMethodName();
+        log.info(String.format("Test %s has skipped", testName));
     }
 
     public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
@@ -59,7 +68,7 @@ public class TestListener implements ITestListener {
                             + getCurrentTimeAsString() +
                             ".png"));
         } catch (IOException e) {
-            log.error("Failed to save screenshot: " + e.getLocalizedMessage());
+            log.error(String.format("Failed to save screenshot: %s", e.getLocalizedMessage()));
         }
     }
 
